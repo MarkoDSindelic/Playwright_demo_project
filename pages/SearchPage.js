7c0ddce2700c6693e96f55638a05388fb6d34cb7
MarkoDSindelic <markods@aia.ac>
2025-07-24 11:35:50 +0200

    changing the locators on both HomePage and SearchPage

--- pages/SearchPage.js
+++ pages/SearchPage.js
@@ -3,21 +3,14 @@ export class SearchPage {
     this.page = page;
 
     this.searches = page.getByRole("button", { name: "Searches" });
-
     this.ticketType = page.locator(
-      "#navbarSupportedContent > div.nav-item--right > ul > li.nav-item.dropdown.w-100 > ul > li:nth-child(2) > a > div > small:nth-child(1)"
-    );
-
-    this.TicketTypeTest = page.locator(
       '//*[@id="navbarSupportedContent"]/div[2]/ul/li[1]/ul/li[2]/a/div/small[1]'
     );
 
     this.date = page.locator(
-      "#navbarSupportedContent > div.nav-item--right > ul > li.nav-item.dropdown.w-100 > ul > li:nth-child(2) > a > div > small:nth-child(2)"
+      '//*[@id="navbarSupportedContent"]/div[2]/ul/li[1]/ul/li[2]/a/div/small[2]'
     );
-    this.adultTravellers = page.locator(
-      "#navbarSupportedContent > div.nav-item--right > ul > li.nav-item.dropdown.w-100 > ul > li:nth-child(2) > a > div > small:nth-child(3) > strong"
-    );
+    this.adultTravellers = page.getByText("Travellers 2 Adults");
     this.fromTo = page.locator(
       "#fadein > main > section > div:nth-child(1) > div.new-m-main-tit__content.flex1.my-2 > div.flex.tit-travel-restriction-wrapper > span > small:nth-child(1) > strong"
     );
